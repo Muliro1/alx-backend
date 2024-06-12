e7634d30af57ac26a081e70064f019e310192e81
Muliro1 <mulirokhaemba@gmail.com>
2024-06-12 17:46:28 +0300

    more

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -3,22 +3,34 @@ import { createQueue } from 'kue';
 const blacklistedNumbers = ['4153518780', '4153518781'];
 const queue = createQueue();
 
+
 /**
- * Handles notification jobs for queue 'push_notification_code_2'
- * @param {string} phoneNumber - user phone number
- * @param {string} message - push notification message
- * @param {import('kue').Job}} job - queue job
- * @param {import('kue').DoneCallback} done - done call back
+ * Handles notification jobs for the 'push_notification_code_2' queue.
+ *
+ * @param {string} phoneNumber - The user's phone number.
+ * @param {string} message - The push notification message.
+ * @param {import('kue').Job} job - The queue job.
+ * @param {import('kue').DoneCallback} done - The done callback.
  * @returns {void}
  */
 function sendNotification(phoneNumber, message, job, done) {
+  // Update the job progress to 0%
   job.progress(0, 100);
+
+  // Check if the phone number is blacklisted
   if (blacklistedNumbers.some((number) => number === phoneNumber)) {
+    // If the phone number is blacklisted, call the done callback with an error
     done(new Error(`Phone number ${phoneNumber} is blacklisted`));
     return;
   }
+
+  // Update the job progress to 50%
   job.progress(50, 100);
+
+  // Log the notification details
   console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+
+  // Call the done callback to indicate that the job is complete
   done();
 }
 
